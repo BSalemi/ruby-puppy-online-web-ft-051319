9accf96972f5ea5f0951aa1a5ab0dd7643ece5db
Brian Salemi <briandsalemi@gmail.com>
2019-05-20 21:09:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,15 @@
 class Dog
   attr_accessor :name
 
-  @@all_dogs = []
+  @@all = []
 
   def initialize(name)
     @name = name
-    @@all_dogs << self
+    @@all << self
   end
 
   def self.all
-    @@all_dogs.each do |dog|
+    @@all.each do |dog|
       puts dog
     end
   end
